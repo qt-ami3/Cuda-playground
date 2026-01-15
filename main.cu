2d1dbdb211d6a163692e7927dd63e70a5e8002e9
aval <riverknuuttila2@outlook.com>
2026-01-15 08:30:35 -0500

    added hello world program

---
+++ main.cu
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+__global__ void add(int* a, int* b, int* c) {
+  int i = threadIdx.x + blockDim.x * blockIdx.x;
+  c[i] = a[i] + b[i];
+}
+
+__managed__ int vector_a[256], vector_b[256], vector_c[256];
+
+int main() {
+  for (int i = 0; i < 256; i++) {
+    vector_a[i] = i;
+    vector_b[i] = 256 - i;
+  }
+
+  add<<<1, 256>>>(vector_a, vector_b, vector_c);
+
+  cudaDeviceSynchronize();
+
+  int result_sum = 0;
+
+  for (int i = 0; i < 256; i++){
+    result_sum += vector_c[i];
+  }
+
+  printf("Hi Mom! I added vectors on a imaginary GPU :3");
+  printf("Result: sum = % d", result_sum);
+}
